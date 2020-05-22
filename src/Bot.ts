8f84116c6a930fd3188588c2c7a7686355718899
aaronsalva27 <aaronsalva27@gmail.com>
2020-05-22 19:28:19 +0200

    Add prefix to commands

--- src/Bot.ts
+++ src/Bot.ts
@@ -3,16 +3,23 @@ import { Client, Message } from "discord.js";
 
 dotenv.config();
 
-const client = new Client();
+const client = new Client()
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`)
 });
 
-client.on("message", (msg: Message) => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!");
+client.on("message", (message: Message) => {
+  const prefix = process.env.PREFIX;
+
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    message.reply("Pong!")
   }
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
